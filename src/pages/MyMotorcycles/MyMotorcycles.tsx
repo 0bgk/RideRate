58aff6e75b97d1bd460242f3acabbc5d37cdeb8b
0bgk <orlandohector41@gmail.com>
2025-08-24 19:59:57 +0200

    feat: my motorcycle page added

---
+++ src/pages/MyMotorcycles/MyMotorcycles.tsx
@@ -0,0 +1,46 @@
+import MotorcycleCard from '../../components/MotorcycleCard/MotorcycleCard'
+import Search from '../../components/Search/Search'
+import styles from './MyMotorcycles.module.css'
+import { useMotorcycles } from '../../hooks/useMotorcycles'
+
+const MyMotorcycles = () => {
+  const {
+    motorcycles,
+    filteredMotorcycles,
+    inputValue,
+    setInputValue,
+    setSearch,
+    visibleCount,
+    showMore,
+  } = useMotorcycles()
+
+  return (
+    <div className={styles.container}>
+      <Search
+        search={inputValue}
+        setSearch={setInputValue}
+        motorcycles={motorcycles}
+        onSearch={() => setSearch(inputValue)} 
+      />
+
+      <section aria-labelledby="my-bikes-heading" className={styles.motorcyclesSection}>
+        <h2 id="my-bikes-heading" className={styles.heading}>My Motorcycles</h2>
+        <ul className={styles.motorcycleGrid}>
+          {filteredMotorcycles.slice(0, visibleCount).map(motorcycle => (
+            <MotorcycleCard key={motorcycle.id} {...motorcycle} /> 
+          ))}
+        </ul>
+
+        {visibleCount < filteredMotorcycles.length && (
+          <div className={styles.showMoreContainer}>
+            <button className={styles.showMoreBtn} onClick={showMore}>
+              Show More
+            </button>
+          </div>
+        )}
+      </section>
+    </div>
+  )
+}
+
+export default MyMotorcycles
